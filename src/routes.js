f77554ebe319b0deda50bb1cd619ecf4ffa74539
Harry MacCode <emarh.harry.code@gmail.com>
2024-02-04 23:32:56 +0100

    The Sign in form

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route }  from 'react-router-dom';
 import Home from './components/home/Home';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
+import SignIn from './components/signIn/SignIn';
 
 const MyRoutes = () => {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
+        <Route path='/sign-in' element={<SignIn />} />
         <Route path='/' element={<Home />} />
       </Routes>
       <Footer />
